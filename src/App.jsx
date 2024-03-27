00ecf9c8001d1841c5e6b4b53f30a37fb0c400cf
juanfedileo <juanfedileo@gmail.com>
2024-03-26 23:40:58 -0300

    eslint y primer div
    BUSCAR COMO AUTO IMPORTAR!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import React from "react";
 import "./App.css";
-
+import NavBar from "./components/NavBar.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <NavBar />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NavBar = () => {
+  return <div className="w-20 h-20 bg-white">NavBar</div>;
+};
